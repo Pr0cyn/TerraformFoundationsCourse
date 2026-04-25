a28d387e679535144775e5c8cf6d4471b6ecab02
Pr0cyn <levi.miklos@google.com>
2026-04-25 20:36:39 +0200

    Add public subnet

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "dev_vpc" {
     Name = "dev-vpc"   
   }
 }
+
+
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id = aws_vpc.dev_vpc.id
+  cidr_block = "10.10.1.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev_public_subnet"
+  }
+}
